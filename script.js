006d7311d09f9de7fa512a346f98e8621c8ddb3f
Kaviyaselvakumar <117004949+Kaviyaselvakumar@users.noreply.github.com>
2023-04-20 18:37:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function displayTime(){
+    let dateTime = new Date();
+    let hr=dateTime.getHours();
+    let min=dateTime.getMinutes();
+    let sec=dateTime.getSeconds();
+    document.getElementById('hours').innerHTML = hr
+    document.getElementById('mins').innerHTML = min
+    document.getElementById('seconds').innerHTML = sec
+}
+setInterval(displayTime, 500)
